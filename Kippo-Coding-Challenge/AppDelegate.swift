dac1ff42807117f306a36356d7d71cef2b22d6f4
garenk95 <garen.kantarci1@gmail.com>
2021-06-16 13:31:57 -0700

    imported assets for tab bar icons and edited tab bar settings in AppDelegate

--- Kippo-Coding-Challenge/AppDelegate.swift
+++ Kippo-Coding-Challenge/AppDelegate.swift
@@ -14,6 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        UITabBar.appearance().barTintColor = .darkGray
+        UITabBar.appearance().tintColor = .white
         // Override point for customization after application launch.
         return true
     }
