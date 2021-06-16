e8b743c1637f2b8230f1ff8979a13a6adbc7cd82
garenk95 <garen.kantarci1@gmail.com>
2021-06-16 10:17:54 -0700

    connected view controller files to view controllers in storyboard

--- Kippo-Coding-Challenge/TrendingViewController.swift
+++ Kippo-Coding-Challenge/TrendingViewController.swift
@@ -12,6 +12,7 @@ class TrendingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
     
