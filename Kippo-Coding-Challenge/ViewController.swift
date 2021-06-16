0a699bd0e5c1411847421384e8b3390566051b44
garenk95 <garen.kantarci1@gmail.com>
2021-06-16 09:10:21 -0700

    Initial Commit

---
+++ Kippo-Coding-Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kippo-Coding-Challenge
+//
+//  Created by Garen Kantarci on 6/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
